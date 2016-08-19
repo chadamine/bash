da0e0fe1d267eeee04c825fbd32e610f1195f0b2
chadamine <chadwicklew@gmail.com>
2016-08-19 14:52:33 -0600

    chop test

---
+++ chop_works.sh
@@ -0,0 +1,20 @@
+echo "This script will remove http data before each line of text in an input file as well as the .[filetype] ending following the image name, preserving original file structure"
+
+read -p "Input file: " file
+#read -p "\n\nVerbose output?(1|0) " ver
+
+sed -ri 's/http:\/\/media-cache-ec0.pinimg.com\///' $file
+# ;w /dev/stdout' $file
+
+sed -ri 's/http:\/\/media-cache-ak0.pinimg.com\///' $file
+# ;w /dev/stdout' $file
+
+#sed -ri 's/[a-z0-9]\{15,\}.jpg//' $file
+#;w /dev/stdout' $file 
+
+sed -ri 's/.jpg//' $file #| awk -F'/' '{print $1// $2// $3}'
+# ;w /dev/stdout' $file
+
+sed -ri 's/.{32}$//' $file
+#sed -ri '/^.$/' $file | awk -F'/' '{print $1// $2// $3}' 
+
